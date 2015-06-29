bb298c709599ba2ed68e1d3e836ae8771784d88c
Matthew Di Nardo <matthew.dinardo@gmail.com>
2015-06-29 12:10:35 -0400

    Retabbed file.  Added optional input parameter for target device.  Patched input call for python 2.x

--- ds1000z.py
+++ ds1000z.py
@@ -18,86 +18,90 @@ import datetime
 
 # pyvisa
 try:
-  import visa
+    import visa
 except ImportError:
-  print("pyVISA not installed")
-  sys.exit()
+    print("pyVISA not installed")
+    sys.exit()
 
 
 
-def screenshot():
-  """
-  
-  """
-  rm = visa.ResourceManager()
-  
-  res = rm.list_resources()
-  #print( res )
-  ins = rm.get_instrument( res[0] ) # !
-  
-  '''
-  idn = ins.ask("*IDN?")
-  idns = idn.split(',')
-  print( "Manufacturer:  ", idns[0] )
-  print( "Device:     ", idns[1] )
-  print( "Serial number:  ", idns[2] )
-  print( "Firmware version:", idns[3] )
-  '''
-  
-  ins.write(":DISP:DATA?")
-  bmpdata = ins.read_raw()[2+9:]
-  
-  ins.close()
-  
-  # save image file
-  name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-  print("Enter a filename-safe description (or none). Hit return.")
-  comment = input( name + '_' )
-  if( len(comment) ):
-    name = name + "_" + comment
+def screenshot(device=None):
+    """
     
-  try:
-    from PIL import Image
-  
-  except ImportError as e:
-    print("PIL(low) not imported because:", e)
-    filename = name + ".bmp"
-    print("Saving screen as", filename)
-    with open( filename, "wb") as f:
-      f.write( bmpdata )
-  
-  else:
-    print( "PILlow", sys.version)
-    import io
-    filename = name + ".png"
-    print("Saving screen as", filename)
-    im = Image.open( io.BytesIO(bmpdata) )
+    """
+    rm = visa.ResourceManager()
     
-    #logo = ((5,10),(74,16))
-    #measure = ((0,37),(60,414))
+    res = rm.list_resources()
+    #print( res )
+    ins = rm.get_instrument( res[0] if not device else device ) 
+    
+    '''
+    idn = ins.ask("*IDN?")
+    idns = idn.split(',')
+    print( "Manufacturer:  ", idns[0] )
+    print( "Device:     ", idns[1] )
+    print( "Serial number:  ", idns[2] )
+    print( "Firmware version:", idns[3] )
+    '''
+    
+    ins.write(":DISP:DATA?")
+    bmpdata = ins.read_raw()[2+9:]
+    
+    ins.close()
+    
+    # save image file
+    name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    print("Enter a filename-safe description (or none). Hit return.")
+    comment = raw_input( name + '_' )
+    if( len(comment) ):
+        name = name + "_" + comment
+        
     try:
-      overlay = Image.open( "overlay050.png" )
-    except IOError as e:
-      print("Overlay image file could not be opened because:", e)
-    else:
-      im.putalpha(255)
-      im = Image.alpha_composite(im, overlay)
+        from PIL import Image
     
-    print("writing file")
-    im.save( filename )
-  
-  try:
-    import os
-    os.startfile(filename)
-  except AttributeError as e:
-    print("Could not open image file in default application because:", e)
+    except ImportError as e:
+        print("PIL(low) not imported because:", e)
+        filename = name + ".bmp"
+        print("Saving screen as", filename)
+        with open( filename, "wb") as f:
+            f.write( bmpdata )
+    
+    else:
+        print( "PILlow", sys.version)
+        import io
+        filename = name + ".png"
+        print("Saving screen as", filename)
+        im = Image.open( io.BytesIO(bmpdata) )
+        
+        #logo = ((5,10),(74,16))
+        #measure = ((0,37),(60,414))
+        try:
+            overlay = Image.open( "overlay050.png" )
+        except IOError as e:
+            print("Overlay image file could not be opened because:", e)
+        else:
+            im.putalpha(255)
+            im = Image.alpha_composite(im, overlay)
+        
+        print("writing file")
+        im.save( filename )
+    
+    try:
+        import os
+        os.startfile(filename)
+    except AttributeError as e:
+        print("Could not open image file in default application because:", e)
 
 if __name__ == "__main__":
-  
-  print( "Python", sys.version, "on", sys.platform )
-  print( "Pyvisa", visa.__version__ )
-  
-  screenshot()
-  
-  print( "done." )
+    
+    print( "Python", sys.version, "on", sys.platform )
+    print( "Pyvisa", visa.__version__ )
+ 
+    device = None
+    if len(sys.argv) >= 2:
+        device = sys.argv[1]
+    
+    screenshot(device)
+    
+    print( "done." )
 
